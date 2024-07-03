9e82b2c6fb57cc6c0c9724c40bca984060c29dd3
Joel <sygullajoel@gmail.com>
2024-07-03 15:38:57 +0200

    Add first objects and drawing logic

--- models/world.class.js
+++ models/world.class.js
@@ -1,13 +1,48 @@
 class World {
     character = new Character();
     enemies = [
-    new Chicken(),
-    new Chicken(),
-    new Chicken(),
-];
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
+    ];
+    clouds = [
+        new Cloud()
+    ];
+    backgroundObjects = [
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0, 100)
+    ];
+    canvas;
+    ctx;
+
+    constructor(canvas){
+        this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
+        this.draw();
+    }
 
 
     draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
 
+        this.addToMap(this.character);
+        this.addObjectsToMap(this.clouds);
+        this.addObjectsToMap(this.enemies);
+        this.addObjectsToMap(this.backgroundObjects);
+
+        // // Draw() wird immer wieder aufgerufen
+        // requestAnimationFrame(() => {
+        //     this.draw();
+        // });
     }
+
+    addObjectsToMap(objects){
+        objects.forEach(o => {
+            this.addToMap(o);
+        });
+    }
+
+    addToMap(mo) {
+        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+    }
+
 }
