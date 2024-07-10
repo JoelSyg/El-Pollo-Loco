6ee99e8fc88b3d3e9f59af2f7e0753aedd68afdc
Joel <sygullajoel@gmail.com>
2024-07-10 19:26:29 +0200

    feat: Add idle and long idle character animations for inactivity

--- models/chracter.class.js
+++ models/chracter.class.js
@@ -13,6 +13,31 @@ class Character extends MovableObject {
     right: 0
   }
 
+  IMAGES_IDLE = [
+    'img/2_character_pepe/1_idle/idle/I-1.png',
+    'img/2_character_pepe/1_idle/idle/I-2.png',
+    'img/2_character_pepe/1_idle/idle/I-3.png',
+    'img/2_character_pepe/1_idle/idle/I-4.png',
+    'img/2_character_pepe/1_idle/idle/I-5.png',
+    'img/2_character_pepe/1_idle/idle/I-6.png',
+    'img/2_character_pepe/1_idle/idle/I-7.png',
+    'img/2_character_pepe/1_idle/idle/I-8.png',
+    'img/2_character_pepe/1_idle/idle/I-9.png',
+    'img/2_character_pepe/1_idle/idle/I-10.png'
+  ]
+
+  IMAGES_LONG_IDLE = [
+    'img/2_character_pepe/1_idle/long_idle/I-11.png',
+    'img/2_character_pepe/1_idle/long_idle/I-12.png',
+    'img/2_character_pepe/1_idle/long_idle/I-13.png',
+    'img/2_character_pepe/1_idle/long_idle/I-14.png',
+    'img/2_character_pepe/1_idle/long_idle/I-15.png',
+    'img/2_character_pepe/1_idle/long_idle/I-16.png',
+    'img/2_character_pepe/1_idle/long_idle/I-17.png',
+    'img/2_character_pepe/1_idle/long_idle/I-18.png',
+    'img/2_character_pepe/1_idle/long_idle/I-19.png',
+    'img/2_character_pepe/1_idle/long_idle/I-20.png'
+  ]
 
   IMAGES_WALKING = [
     "/img/2_character_pepe/2_walk/W-21.png",
@@ -45,7 +70,7 @@ class Character extends MovableObject {
     "img/2_character_pepe/5_dead/D-57.png"
   ]
 
-  IMAGES_HURT= [
+  IMAGES_HURT = [
     "img/2_character_pepe/4_hurt/H-41.png",
     "img/2_character_pepe/4_hurt/H-42.png",
     "img/2_character_pepe/4_hurt/H-43.png"
@@ -53,6 +78,7 @@ class Character extends MovableObject {
 
   world;
   walking_sound = new Audio("audio/running.mp3");
+  idleStartTime = null;
 
   constructor() {
     super().loadImage("/img/2_character_pepe/2_walk/W-21.png");
@@ -60,11 +86,18 @@ class Character extends MovableObject {
     this.loadImages(this.IMAGES_JUMPING);
     this.loadImages(this.IMAGES_DEAD);
     this.loadImages(this.IMAGES_HURT);
+    this.loadImages(this.IMAGES_IDLE);
+    this.loadImages(this.IMAGES_LONG_IDLE);
     this.applyGravity();
     this.animate();
   }
 
+  resetIdleTimer() {
+    this.idleStartTime = new Date().getTime();
+  }
+
   animate() {
+    this.resetIdleTimer();
 
     setInterval(() => {
       this.walking_sound.pause();
@@ -72,36 +105,44 @@ class Character extends MovableObject {
         this.moveRight();
         this.walking_sound.play();
         this.otherDirection = false;
+        this.resetIdleTimer();
       }
 
       if (this.world.keyboard.LEFT && this.x > 0) {
         this.moveLeft();
         this.walking_sound.play();
         this.otherDirection = true;
+        this.resetIdleTimer();
       }
 
-      if(this.world.keyboard.UP && !this.isAboveGround()) {
+      if (this.world.keyboard.UP && !this.isAboveGround()) {
         this.jump();
+        this.resetIdleTimer();
       }
 
       this.world.camera_x = -this.x + 100;
     }, 1000 / 60);
 
     setInterval(() => {
-
       if (this.isDead()) {
-          this.playAnimation(this.IMAGES_DEAD);
+        this.playAnimation(this.IMAGES_DEAD);
       } else if (this.isHurt()) {
         this.playAnimation(this.IMAGES_HURT);
       } else if (this.isAboveGround()) {
         this.playAnimation(this.IMAGES_JUMPING);
       } else {
-
         if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
           this.playAnimation(this.IMAGES_WALKING);
+        } else {
+          const idleTime = new Date().getTime() - this.idleStartTime;
+          if (idleTime > 3000) {
+            this.playAnimation(this.IMAGES_LONG_IDLE);
+          } else {
+            this.playAnimation(this.IMAGES_IDLE);
+          }
+        }
       }
-    }
-    }, 50);
+    }, 80);
   }
-
 }
+
